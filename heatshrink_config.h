cf564c38a151edf71ee0c63506359a0a1807af18
Scott Vokes <scott.vokes@atomicobject.com>
2013-05-15 17:17:04 -0400

    Reduce default static_input_buffer_size.
    There seems to be diminishing returns after 32 bytes, and it takes
    significantly less space. (It's only a default, anyway.)

--- heatshrink_config.h
+++ heatshrink_config.h
@@ -10,7 +10,7 @@
     #define HEATSHRINK_FREE(P, SZ) free(P)
 #else
     /* Required parameters for static configuration */
-    #define HEATSHRINK_STATIC_INPUT_BUFFER_SIZE 256
+    #define HEATSHRINK_STATIC_INPUT_BUFFER_SIZE 32
     #define HEATSHRINK_STATIC_WINDOW_BITS 8
     #define HEATSHRINK_STATIC_LOOKAHEAD_BITS 4
 #endif
